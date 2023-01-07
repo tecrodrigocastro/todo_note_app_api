a025c1104cee84a4f15b940c2f7687e6bd1af1d3
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2023-01-07 16:04:30 -0300

    INITIAL COMMIT

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+    public function success(mixed $data, string $message = "okay", int $statusCode = 200): JsonResponse
+    {
+        return response()->json([
+            'data' => $data,
+            'success' => true,
+            'message' => $message,
+        ], $statusCode);
+    }
+}
