24906eecb57bba4d4b589c44b9e7d2704dc190f8
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2023-01-07 16:34:51 -0300

    Note Factory

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Note>
+ */
+class NoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title'=>$this->faker->jobTitle,
+            'content'=>$this->faker->words(50,true),
+            'color'=>$this->faker->safeHexColor,
+        ];
+    }
+}
